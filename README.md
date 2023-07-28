97cbf362f6c6a9b28d50c892c634e32ba13cd25f
Paulo César Moraes de Menezes <paulo.moraes@sou.unifal-mg.edu.br>
2023-07-28 11:14:06 -0300

    Projeto final

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Projeto-automacao-de-emails
+
+adicione um arquivo .env contendo seu login e senha, caso deseja enviar emails, ative a verificação de duas etapas e chame a função, adicione o codigo no arquivo .env
+adicione o chromium também, estou usando o linux e o chrome, varia de navegador e so
--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 import subprocess
+from selenium .common.exceptions import TimeoutException
 
 CAMINHO_MENSAGEM = Path(__file__).parent / 'mensagem.txt'
 
@@ -47,108 +48,39 @@ def enviar_email():
         server.send_message(mime_multipart)
         print('Email enviado com sucesso')
 
-# enviar_email()
-
 CAMINHO_ENV = Path(__file__).parent / '.env'
-ROOT_FOLDER = Path(__file__).parent 
+ROOT_FOLDER = Path(__file__).parent
 CHROME_DRIVER_EXE = ROOT_FOLDER / 'driver' / 'chromedriver'
+
 chrome_options = webdriver.ChromeOptions()
-chrome_services = Service(executable_path=CHROME_DRIVER_EXE)
-chrome_browser = webdriver.Chrome(
-        service= chrome_services,
-        options= chrome_options,
-    )
+chrome_services = webdriver.chrome.service.Service(executable_path=CHROME_DRIVER_EXE)
+chrome_browser = webdriver.Chrome(service=chrome_services, options=chrome_options)
 
 def caminho_email():
-
     if os.path.isfile(CAMINHO_ENV):
         load_dotenv()
         LOGIN = os.getenv('LOGIN')
         SENHA = os.getenv('SENHA')
     else:
-        print('Não encontrado')
+        print('Arquivo .env não encontrado')
+        return
+    try:
+        # Abre a página de login do Gmail
+        chrome_browser.get('https://accounts.google.com/v3/signin/identifier?dsh=S-1123588098%3A1690459661043675&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=AeDOFXhVE1IFRojL6avwC9M5fC8ZsPojtAW5phfesRg5s9FqrvbZh3Yq14LAK5iYpRUv9e3Uk1ss&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
 
-    # print(CHROME_DRIVER_EXE)
+        # Realiza o login
+        logar = WebDriverWait(chrome_browser, 10).until(EC.presence_of_element_located((By.ID, 'identifierId')))
+        logar.send_keys(LOGIN)
+        logar.send_keys(Keys.ENTER)
 
+        time.sleep(3)
 
-    chrome_browser.get('https://accounts.google.com/v3/signin/identifier?dsh=S-1123588098%3A1690459661043675&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=AeDOFXhVE1IFRojL6avwC9M5fC8ZsPojtAW5phfesRg5s9FqrvbZh3Yq14LAK5iYpRUv9e3Uk1ss&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+        colocar_senha = WebDriverWait(chrome_browser, 10).until(EC.presence_of_element_located((By.NAME, 'Passwd')))
+        colocar_senha.send_keys(SENHA)
+        colocar_senha.send_keys(Keys.ENTER)
+        time.sleep(30)
+    except:
+        print('Erro')
+caminho_email()
 
-    logar = WebDriverWait(chrome_browser, 5).until(
-        EC.presence_of_element_located(
-            (By.ID, 'identifierId')
-        )
-    )
-
-    logar.send_keys(LOGIN)
-    logar.send_keys(Keys.ENTER)
-
-    time.sleep(3)
-
-    colocar_senha = WebDriverWait(chrome_browser, 5).until(
-        EC.presence_of_element_located(
-            (By.NAME, 'Passwd')
-        )
-    )
-    colocar_senha.send_keys(SENHA)
-    colocar_senha.send_keys(Keys.ENTER)
-    time.sleep(5)
-    verificar.navegador_aberto = True
-
-
-#caminho_email()
-
-class Estado:
-        verifica_duas_etapas = False
-        navegador_aberto = False
-
-verificar = Estado()
-
-def habilitar_verificacao_duas_etapas():
-    caminho_email()
-    time.sleep(5)
-
-    # Navigate to the desired page where the element with class name "VfPpkd-dgl2Hf-ppHlrf-sM5MNb" is present
-    chrome_browser.get('https://myaccount.google.com/signinoptions/two-step-verification/enroll-welcome?hl=pt_BR')
-    time.sleep(5)
-
-    # Now, find and click the element with the class name "VfPpkd-dgl2Hf-ppHlrf-sM5MNb"
-    habilitar = chrome_browser.find_element(By.CLASS_NAME,"VfPpkd-dgl2Hf-ppHlrf-sM5MNb")
-    habilitar.click()
-    time.sleep(5)
-    habilitar = chrome_browser.find_element(By.CLASS_NAME, "A9wyqf")
-    habilitar.click()
-    time.sleep(10)
-    codigo = int(input('Informe o código de verificação: '))
-    print(codigo)
-    habilitar = WebDriverWait(chrome_browser, 5).until(
-         EC.presence_of_element_located(
-         (By.TAG_NAME, "input")
-         )
-    )
-    habilitar.send_keys(codigo)
-    habilitar.send_keys(Keys.ENTER)
-        
-    
-navegar = 0
-print('-'*40
-          , 'Bem vindo ao sistema de automatização de emails', '-'*40)
-print('Para acessar o gmail, pressione (1)\n'
-      'Para enviar email, pressione (2)\n'
-      'Para sair, pressione (3)')
-
-navegar = int(input())
-if navegar == 1:
-        caminho_email()
-        subprocess.run('clear')
-        print('Email acessado com sucesso!')
-        
-if navegar ==  2:
-    #Chamar a função acessa email, habilitar a verificação de duas etapas, enviar o email, desabilitar a verificação de duas etapas, encerrar programa
-    subprocess.run('clear')
-    habilitar_verificacao_duas_etapas()
-
-if navegar == 3:
-    teste = int(input('Informe o valor'))
-    if teste == 770896:
-         print('ok')
 
